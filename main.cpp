5504bbff1da18bdf5f0d7d1129bad5666d854302
I573040 <elias.rw2@gmail.com>
2023-09-08 08:22:27 +0200

    Added sense

--- main.cpp
+++ main.cpp
@@ -2,33 +2,33 @@
 
 void callByValue(int i){
     std::cout << "Eingabewert " << i << "\n";
-    std::cout << "Erhöterwert " << ++i << "\n";
+    std::cout << "Erhöhter Wert " << ++i << "\n";
 }
 
 
 void callByReference(int &i){
     std::cout << "Eingabewert " << i << "\n";
-    std::cout << "Erhöterwert " << ++i << "\n";
+    std::cout << "Erhöhter Wert " << ++i << "\n";
 }
 
 void callByAddress(int *i){
     std::cout << "Eingabewert " << i << "\n";
-    std::cout << "Erhöterwert " << ++(*i) << "\n";
+    std::cout << "Erhöhter Wert " << ++(*i) << "\n";
 }
 
 int main() {
     int i = 0;
     std::cout << "Call by Value:\n";
     callByValue(i);
-    std::cout << "Wert nach veränderung " << i << "\n";
+    std::cout << "Wert nach Veränderung " << i << "\n";
 
     std::cout << "Call by Reference:\n";
     callByReference(i);
-    std::cout << "Wert nach veränderung " << i << "\n";
+    std::cout << "Wert nach Veränderung " << i << "\n";
 
     std::cout << "Call by address:\n";
     callByAddress(&i);
-    std::cout << "Wert nach veränderung " << i << "\n";
+    std::cout << "Wert nach Veränderung " << i << "\n";
 
     return 0;
 }
