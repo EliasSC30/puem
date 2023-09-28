58eb566964e24c557a9e815ee37f583a819311d8
Collin Forslund <collin.forslund@sap.com>
2023-09-28 10:06:35 +0200

    added something

---
+++ DoublyLinkedList.h
@@ -0,0 +1,28 @@
+//
+// Created by Forslund, Collin on 28.09.23.
+//
+
+#ifndef PUEM_DOUBLYLINKEDLIST_H
+#define PUEM_DOUBLYLINKEDLIST_H
+
+
+class DoublyLinkedList {
+    class Node{
+        Node* ptr;
+        int data;
+    public:
+        Node(int data) : data(data){}
+        Node next(Node* prev){
+
+        }
+
+        Node previous(Node* next){
+
+        }
+    };
+
+
+};
+
+
+#endif //PUEM_DOUBLYLINKEDLIST_H
